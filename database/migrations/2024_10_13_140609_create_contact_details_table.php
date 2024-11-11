04c0e916021d9b2e48538ef2db1d448b59406c11
Ammar Hammoud <st.ammar.hammoud@gmail.com>
2024-11-11 19:53:13 +0300

    x

--- database/migrations/2024_10_13_140609_create_contact_details_table.php
+++ database/migrations/2024_10_13_140609_create_contact_details_table.php
@@ -14,14 +14,14 @@ return new class extends Migration
     {
         Schema::create('contact_details', function (Blueprint $table) {
             $table->id();
-            $table->string('first_name');
-            $table->string('last_name');
-            $table->string('email');
-            $table->string('phone_number');
-            $table->text('address');
-            $table->unsignedBigInteger('booking_id'); // Foreign key
-            $table->foreignIdFor(Booking::class)->constrained();
-            $table->timestamps();
+            // $table->string('first_name');
+            // $table->string('last_name');
+            // $table->string('email');
+            // $table->string('phone_number');
+            // $table->text('address');
+            // $table->unsignedBigInteger('booking_id'); // Foreign key
+            // $table->foreignIdFor(Booking::class)->constrained();
+            // $table->timestamps();
         });
     }
 
